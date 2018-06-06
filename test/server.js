bf92e2d56d4f960cd799fbeb46c9964c194871e1
liejiayong <809206619@qq.com>
2018-06-06 23:23:09 +0800

    update teset

--- test/server.js
+++ test/server.js
@@ -1,8 +1,20 @@
 const http = require('http')
+const url = require('url')
 
-http.createServer(function(require, response) {
-  response.writeHead(200, {'content-Type': 'text/plain'})
-  response.end('Hello world!')
-}).listen(8888)
+function start(route) {
+  function onRequest(request, response) {
+    let pathname = url.parse(request.url).pathname
+    console.log('Request for' + pathname + 'received.')
 
-console.log("Server running at http://127.0.0.1:8888/")
+    route(pathname)
+
+    response.writeHead(200, {"Content-Type": "text/plain"})
+    response.write('hello world')
+    response.end()
+  }
+
+  http.createServer(onRequest).listen(8888)
+  console.log('Server has started.')
+}
+
+exports.start = start
